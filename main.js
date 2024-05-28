f952a3d9b131fd82c306e82df460b657cc2a1ae9
MeznahAldossari <MeznahAldossari@hotmail.com>
2024-05-28 16:48:58 +0300

    v1

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ let input = document.getElementById("urlInput");
 function firstSize(w, h) {
   url = input.value;
 
-  //   window.open(url, "", "width=" + w + ", height=" + h);
   window.open(url, "", "width=" + w + ", height=" + h);
 }
